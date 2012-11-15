29f70f4c5aa92b9e698a1036b1df9553727aa950
Anatoliy Lysenko <a.o.lysenko@gmail.com>
2012-11-15 14:30:44 +0200

    Yet another interview test is finished.

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
 test
 ====
 
-Yet another job interview test
+Yet another job interview test.
+
+Write a program in Ruby that takes one command line argument (referred to going forward as n). If n is not a perfect square print an appropriate error message and exit. If n is a perfect square then create a sequence from 1 to  n and build a matrix from the sequence by walking in counter-clockwise spiral order. Examples below illustrate this for n=1,4,9,16 but your solution should work for any n that is a perfect square. Once you have built the matrix print it, ensuring even column widths as in examples below and then exit.
+
+Input:  1
+Output: 1
+
+Input:  4
+Output: 4 3
+          1 2
+
+Input:  9
+Output: 5 4 3
+        6 1 2
+        7 8 9
+
+Input:  16
+Output: 16 15 14 13
+        5  4  3  12
+        6  1  2  11
+        7  8  9  10
---
+++ main.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/ruby
+
+n=ARGV.first
+
+puts "Input: #{n}"
+
+begin
+  n_sqrt = Math.sqrt(n).to_i
+  raise if (n_sqrt * n_sqrt) != n.to_i
+rescue
+  puts "Error: input should be a perfect square number"
+  exit -1
+end
+
+# mathematicaly, zero is perfect square number,
+# because sqrt(0) == 0, and 0*0 == 0
+
+if n_sqrt.zero?
+  puts "Output:" # I just print zero size matrix :)
+  exit 0
+end
+
+# n is the size of the square
+# notice that a caller should ensure that n is valid - KISS
+def counter_clockwise_spiral_square(n)
+  if 1 == n
+    return [[1]]
+  else
+    matrix = counter_clockwise_spiral_square(n-1)
+    sequence = (((n-1)*(n-1)+1)..n*n).to_a
+
+    if n.odd?
+      matrix.each{ |array| array.unshift(sequence.shift)}
+      matrix << sequence
+    else
+      #can be done without reverse, but "Premature optimization is the root of all evil"
+      matrix.reverse.each{ |array| array << sequence.shift }
+      matrix.unshift sequence.reverse
+    end
+    return matrix
+  end
+end
+
+matrix = counter_clockwise_spiral_square(n_sqrt)
+
+cell_width = n.to_s.size
+
+matrix.each do |array|
+  array.each do |element|
+    width = element.to_s.size
+    print " " * (cell_width-width) + element.to_s
+    print " "
+  end
+  print "\n"
+end
+
+exit 0
